883d9d10f23881461dfe6180764b00b2af3314bd
Scott Travis <scott.w.travis@gmail.com>
2012-12-08 14:18:43 -0600

    Updates.

--- libraries/congress.php
+++ libraries/congress.php
@@ -2,84 +2,107 @@
 
 class Congress
 {
-	public $congress, $session, $cycle, $year;
+    public $congress, $session, $cycle, $year;
+    
+    /**
+     * Build congress info based on congress.
+     *
+     * @return  object
+     */
+    protected static function from_congress($congress, $session)
+    {
+        // create
+        $class = __CLASS__;
+        $object = new $class;
 
-	public static function forge()
-	{
-		return static::forge_from_year();
-	}
-	
-	public static function forge_from_congress($congress, $session)
-	{
-		$class = __CLASS__;
-		$object = new $class;
-		return $object->from_congress($congress, $session);
-	}
-	
-	public static function forge_from_year($year = null)
-	{
-		$class = __CLASS__;
-		$object = new $class;
-		return $object->from_year($year);
-	}
-	
-	protected function from_congress($congress, $session)
-	{
-	
-	}
-	
-	protected function from_year($year)
-	{
-		// year default is now
-		if (!$year) $year = (int) strftime('%Y', time());
-		
-		// set what we know
-		$this->year = 2007;
-		$this->congress = 110;
-		$this->session = 1;
-		
-		// loop thru years to target
-		while($this->year !== $year)
-		{
-			if($this->year < $year)
-			{
-				$this->year++;
-				$this->session++;
-			}
-			elseif($this->year > $year)
-			{
-				$this->year--;
-				$this->session--;
-			}
-			if($this->session === 3)
-			{
-				$this->congress++;
-				$this->session = 1;
-			}
-			elseif($this->session === 0)
-			{
-				$this->congress--;
-				$this->session = 2;
-			}
-		}
-		$this->year = $year;
-		
-		// add cycle
-		if($odd = $year%2)
-		{
-			$this->cycle++;
-		}
-		else
-		{
-			$this->cycle = $year;
-		}
-		
-		// return
-		return $this;
-	}
-	
-	public static function filter($string)
-	{
-		return preg_replace('/[.,!?:;\'"-]+/i', '', $string);
-	}
+        // set vars
+        $object->congress = $congress;
+        $object->session = $session;
+
+        // calculate
+        // ...
+
+        // return
+        return $object;
+    }
+    
+    /**
+     * Build congress info based on year.
+     *
+     * @return  object
+     */
+    public static function from_year($year = null)
+    {
+        // create
+        $class = __CLASS__;
+        $object = new $class;
+
+        // set vars
+        if (!$year) $year = (int) strftime('%Y', time());
+        $object->year = $year;
+    
+        // set givens
+        $object->year = 2007;
+        $object->congress = 110;
+        $object->session = 1;
+        
+        // loop thru years to target
+        while($object->year !== $year)
+        {
+            if($object->year < $year)
+            {
+                $object->year++;
+                $object->session++;
+            }
+            elseif($object->year > $year)
+            {
+                $object->year--;
+                $object->session--;
+            }
+            if($object->session === 3)
+            {
+                $object->congress++;
+                $object->session = 1;
+            }
+            elseif($object->session === 0)
+            {
+                $object->congress--;
+                $object->session = 2;
+            }
+        }
+        $object->year = $year;
+        
+        // add cycle
+        if($odd = $year%2)
+        {
+            $object->cycle++;
+        }
+        else
+        {
+            $object->cycle = $year;
+        }
+        
+        // return
+        return $object;
+    }
+
+    /**
+     * Alias of from_year() method.
+     *
+     * @return  object
+     */
+    public static function current()
+    {
+        return static::from_year();
+    }
+    
+    /**
+     * Filter bill ids to standardized format.
+     *
+     * @return  string
+     */
+    public static function filter($string)
+    {
+        return preg_replace('/[.,!?:;\'"-]+/i', '', $string);
+    }
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,24 @@
-# Congress for LaravelPHP #
+# Congress for LaravelPHP
 
-A simple library for calculating the Congress and Session of a given date.
+A simple library for converting dates into congress/session number pairs, and vice versa.
+
+## Usage
+
+```
+// get from year
+$congress = Congress::from_year(2012);
+
+// get from congress
+$congress = Congress::from_congress($congress, $session);
+
+// get current
+$congress = Congress::current();
+```
+
+Returns an object containing ``congress``, ``session``, ``year``, and ``cycle`` values.
+
+## Helper
+
+```
+$bill_number = Congress::filter('H.R. 123'); // returns HR123
+```
